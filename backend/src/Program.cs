d7cf207428de94f53201eda86a78c4bc4629ad8c
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2025-03-25 13:28:25 -0400

    configuring cors

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -6,12 +6,25 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost3000", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Adicione o URL do seu frontend aqui
+            .AllowAnyMethod()                     // Permite qualquer método, incluindo POST
+            .AllowAnyHeader()                     // Permite qualquer cabeçalho (como Content-Type)
+            .AllowCredentials(); 
+    });
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
 
 
 var app = builder.Build();
+
+app.UseCors("AllowLocalhost3000");
 app.ApplyPendingMigrations();
 
 
